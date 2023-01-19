85939e60df82cae74545c43d00ff83e184215801
phucnguyen290198@gmail.com <phucnguyen290198@gmail.com>
2023-01-19 12:32:04 -0600

    add favorite albums on spotify

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -9,7 +9,7 @@ import Layout from "../../components/layout";
 import Navbar from "../../components/navbar";
 import styles from "../../styles/blog.module.css";
 import Link from "next/link";
-import { KeyboardBackspace } from "@mui/icons-material";
+import { KeyboardBackspace, PlayCircleFilled } from "@mui/icons-material";
 
 function srcset(image, size, rows = 1, cols = 1) {
   return {
@@ -57,6 +57,34 @@ export default function Blogs({ data }) {
               dangerouslySetInnerHTML={{ __html: data.content }}
               className={styles.content}
             ></div>
+            {data.songList ? (
+              <ul className={styles.songList}>
+                {data.songList.map((item, i) => (
+                  <li key={i} className={styles.songItem}>
+                    <div className={styles.imgButton}>
+                      <img
+                        src={item.img}
+                        alt={item.title}
+                        className={styles.songImg}
+                      />
+                      <div className={styles.songPlayButton}>
+                        <Link href={item.link}>
+                          <PlayCircleFilled />
+                        </Link>
+                      </div>
+                    </div>
+                    <div className={styles.songContent}>
+                      <div className={styles.songTitleSinger}>
+                        <div className={styles.songTitle}>{item.title}</div>
+                        <div className={styles.songSingers}>{item.singers}</div>
+                      </div>
+                      <div></div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            ) : null}
+
             <p>
               Follow me on{" "}
               <Link href="https://www.instagram.com/philnguyen2901/">
@@ -129,15 +157,72 @@ export async function getStaticProps({ params }) {
       imgURL: "https://i.imgur.com/cs41hM3.jpg",
       content:
         "<p>This is my first blog ever! I am glad I finally decided to create one, due to my laziness, lol. Anyway, I and Nguyet, my girlfriend, just had a blast in one of the most beautiful and intelligent cities in the United States, Seattle, Washington. There is a myriad of attraction such as Space Needle, Museum of Pop Culture (MoPOP), Chihuly Garden and Glass (a must-see IMO), Pike Place Market, Seattle Great Wheel, and so on. Especially, Pike Place Market is famous in Seattle because it's one of the oldest continually running markets in the country, according to travelleming.com. Here, we tried the clam chowder, which is awarded the best clam chowder in the nation. A fun experience in this attraction indeed, not only was because of the food, but also was the people and the environment they brought to the visitors. I remembered when we are offered to try samples of three types of cooked salmon and were amazed at how unique and delicious they were. “Have you ever tried Salmon Candy?”, asked the fisherman. We were confused: “Wait, Salmon Candy? What do you mean? Do they taste sweet?”. Indeed, they were meaty but sweet at the same time, thus our new favorite dessert was introduced. You must try it once when coming to the market. </p><p>We also visited the Space Needle, one of the most famous attractions of the country, if not of the world. Because we travelled either when people were working, or when COVID-19 still spread, not many people were there with us. Therefore, Nguyet and I could enjoy fully the experience of capturing the sunny day (which was rare) in Seattle City from the observatory deck (Like the image above). There are so many pictures we took from the deck that I will create a gallery right here so people can see how beautiful the scene looks like when Seattle weather is nice and peaceful. </p><p>Speaking about Seattle, we could not miss the first Starbucks ever, Starbucks Reserve Roastery, located in 1124 Pike St, Seattle, WA 98101. From the first look outside, the store seemed to be more modern than I imagined. However, the coffee makers, and the roasting process were displayed like it was in the past. We got ourselves our favorite drinks: Peppermint Oatmilk Mocha and Hazelnut Bianco Latte. </p><p>We visited many more attractions, and like I said, I have created a gallery for you to follow and see what they looked like. Oh, and I guess this is the most important thing to consider when traveling to Seattle: You do not even need to rent a vehicle, since Seattle contains tons of public transportation, such as Link Light Rail, Metro Public Bus, Monorail, etc. The transportation is also cheap if you are not planning to stay here long, which only ranges around $3-$8 per trip based on your need. </p><p>Finally, for the budget, we spent the total of around $1000, including round-trip plane tickets, AirBnB, CityPass (you might want to visit this before considering to visit Seattle), and miscellaneous things (food, traveling place to place, shopping,…). </p><p>In conclusion, writing this for the first time was really fun, since I could feel reminiscent about how fun we spent our time in Seattle and ingrain in my memory. I will try to write more of this in the future! Please stay tuned. </p>",
+      songList: null,
     },
     {
       id: 2,
-      title: "My Favorite Book of All Time",
-      date: "1/18/2023",
+      title: "My Favorite Songs for 2023",
+      date: "1/19/2023",
       author: "Phillip Nguyen",
-      imgURL: "/images/AtomicHabits.jpg",
+      imgURL: "/images/spotify.png",
       content:
-        "tortor consequat id porta nibh venenatis cras sed felis eget velit aliquet sagittis id consectetur purus ut faucibus pulvinar elementum integer enim neque volutpat ac tincidunt vitae semper quis lectus nulla at volutpat diam ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt eget nullam non nisi est sit amet facilisis magna etiam tempor orci eu lobortis elementum nibh tellus molestie nunc non blandit massa enim nec dui nunc mattis enim ut tellus elementum sagittis vitae et leo duis ut diam quam nulla porttitor massa id neque aliquam vestibulum morbi blandit cursus risus at ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo integer malesuada nunc vel risus commodo viverra maecenas accumsan lacus vel facilisis volutpat est velit egestas dui id ornare arcu odio ut sem nulla pharetra diam sit amet nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit sed vulputate mi sit amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero nunc consequat interdum varius sit amet mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere ac ut consequat semper viverra nam libero justo laoreet sit amet cursus sit amet dictum sit amet justo donec enim diam vulputate ut pharetra sit amet aliquam id diam maecenas ultricies mi eget mauris pharetra et ultrices neque ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui sapien eget mi proin sed libero enim sed faucibus turpis in eu mi bibendum neque egestas congue quisque egestas diam in arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum ut tristique et egestas quis ipsum suspendisse ultrices gravida dictum fusce ut placerat orci nulla pellentesque dignissim enim sit amet venenatis urna cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque massa placerat duis ultricies lacus sed turpis tincidunt id aliquet risus feugiat in ante metus dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia at quis risus sed vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in dictum non consectetur a erat nam at lectus urna duis convallis convallis tellus id interdum velit laoreet id donec ultrices tincidunt arcu non sodales neque sodales ut etiam sit amet nisl purus in mollis nunc sed id semper risus in hendrerit gravida rutrum quisque non tellus orci ac auctor augue mauris augue neque gravida in fermentum et sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl vel pretium lectus quam id leo in vitae turpis massa",
+        "These are my favorite songs between 2022-2023, hopefully we have the same taste of music :)",
+      songList: [
+        {
+          title: "When I Get Old",
+          singers: "Christopher, CHUNG HA",
+          img: "/images/songs/whenigetold.png",
+          link: "https://open.spotify.com/track/5f2CcxzZoW7hNs1O8NhG6y?si=476de78a032c48c1",
+        },
+        {
+          title: "Dear My Winter",
+          singers: "george, Chuu",
+          img: "/images/songs/dearmywinter.png",
+          link: "https://open.spotify.com/track/7sqtyHfTHtkLtnxuQ3ttvo?si=db6a92f97dd644d1",
+        },
+        {
+          title: "Lovey Dovey",
+          singers: "BIG Naughty, meenoi",
+          img: "/images/songs/loveydovey.png",
+          link: "https://open.spotify.com/track/1s3AJx7XASsPSA2cKJdXG6?si=2c1a58e5903043df",
+        },
+        {
+          title: "Love Song",
+          singers: "Wonstein, JOY",
+          img: "/images/songs/lovesong.png",
+          link: "https://open.spotify.com/track/7JF6USn1d7oBnjITCKtSHp?si=0555b38487324495",
+        },
+        {
+          title: "I Wanna Be - The 1st Album Repackage",
+          singers: "KEY, JEON SOYEON",
+          img: "/images/songs/iwannabe.png",
+          link: "https://open.spotify.com/track/7Bd6h5KwA4ASCXCSoWIS3i?si=9bcf0224f87a4e70",
+        },
+        {
+          title: "I Like Me Better",
+          singers: "Lauv",
+          img: "/images/songs/ilikemebetter.png",
+          link: "https://open.spotify.com/track/0EcQcdcbQeVJn9fknj44Be",
+        },
+        {
+          title: "Stay",
+          singers: "The Kid LAROI, Justin Bieber",
+          img: "/images/songs/stay.png",
+          link: "https://open.spotify.com/track/5HCyWlXZPP0y6Gqq8TgA20?si=ddde4a85cf3045be",
+        },
+        {
+          title: "In the Name of Love",
+          singers: "Martin Garrix",
+          img: "/images/songs/inthenameoflove.png",
+          link: "https://open.spotify.com/track/23L5CiUhw2jV1OIMwthR3S?si=16e454ecf7fe4357",
+        },
+        {
+          title: "Let Me Love You",
+          singers: "DJ Snake, Justin Bieber",
+          img: "/images/songs/letmeloveyou.png",
+          link: "https://open.spotify.com/track/0lYBSQXN6rCTvUZvg9S0lU?si=b7479e32c785404f",
+        },
+      ],
     },
   ];
 
