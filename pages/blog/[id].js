8568fb62c3b0d0447d49d71ec42afd46a881581b
phucnguyen290198@gmail.com <phucnguyen290198@gmail.com>
2023-01-19 17:12:58 -0600

    add favorite albums on spotify - filter by type

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -9,6 +9,7 @@ import Layout from "../../components/layout";
 import Navbar from "../../components/navbar";
 import styles from "../../styles/blog.module.css";
 import Link from "next/link";
+import Checkbox from "@mui/material/Checkbox";
 import { KeyboardBackspace, PlayCircleFilled } from "@mui/icons-material";
 
 function srcset(image, size, rows = 1, cols = 1) {
@@ -22,6 +23,58 @@ function srcset(image, size, rows = 1, cols = 1) {
 
 export default function Blogs({ data }) {
   const [img, setImg] = useState(null);
+  const [checked, setChecked] = useState(true);
+  const [checkedEnglish, setCheckedEnglish] = useState(true);
+  const [filter, setFilter] = useState("");
+
+  let songData = data.songList ? data.songList : null;
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+
+  const handleChangeEnglish = (e) => {
+    setCheckedEnglish(e.target.checked);
+  };
+
+  console.log("checked: " + checked);
+  console.log("checkedEnglish: " + checkedEnglish);
+
+  console.log(data.songList);
+
+  // const toggleFunction = () => {
+  //   if (data.songList) {
+  //     console.log("here");
+  //     if (checked == false && checkedEnglish == true) {
+  //       console.log("English");
+  //       data.songList = data.songList.filter(
+  //         (item) => item.category == "English"
+  //       );
+  //     } else if (checked == true && checkedEnglish == false) {
+  //       console.log("Korean");
+  //       data.songList = data.songList.filter(
+  //         (item) => item.category == "Korean"
+  //       );
+  //     }
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   if (data.songList) {
+  //     console.log("here");
+  //     if (checked == false && checkedEnglish == true) {
+  //       console.log("English");
+  //       data.songList = data.songList.filter(
+  //         (item) => item.category == "English"
+  //       );
+  //     } else if (checked == true && checkedEnglish == false) {
+  //       console.log("Korean");
+  //       data.songList = data.songList.filter(
+  //         (item) => item.category == "Korean"
+  //       );
+  //     }
+  //   }
+  // }, [data, checked, checkedEnglish]);
 
   const matchDownSm = useMediaQuery("(max-width:425px)");
   const matchDownMd = useMediaQuery("(max-width:768px)");
@@ -58,31 +111,122 @@ export default function Blogs({ data }) {
               className={styles.content}
             ></div>
             {data.songList ? (
-              <ul className={styles.songList}>
-                {data.songList.map((item, i) => (
-                  <li key={i} className={styles.songItem}>
-                    <div className={styles.imgButton}>
-                      <img
-                        src={item.img}
-                        alt={item.title}
-                        className={styles.songImg}
-                      />
-                      <div className={styles.songPlayButton}>
-                        <Link href={item.link}>
-                          <PlayCircleFilled />
-                        </Link>
-                      </div>
-                    </div>
-                    <div className={styles.songContent}>
-                      <div className={styles.songTitleSinger}>
-                        <div className={styles.songTitle}>{item.title}</div>
-                        <div className={styles.songSingers}>{item.singers}</div>
-                      </div>
-                      <div></div>
-                    </div>
-                  </li>
-                ))}
-              </ul>
+              <>
+                <Checkbox
+                  checked={checked}
+                  onChange={handleChange}
+                  inputProps={{ "aria-label": "controlled" }}
+                />{" "}
+                K-POP
+                <Checkbox
+                  checked={checkedEnglish}
+                  onChange={handleChangeEnglish}
+                  inputProps={{ "aria-label": "controlled" }}
+                />{" "}
+                US-UK
+                <ul className={styles.songList}>
+                  {((checked && checkedEnglish) ||
+                    (!checked && !checkedEnglish)) && (
+                    <>
+                      {data.songList.map((item, i) => (
+                        <li key={i} className={styles.songItem}>
+                          <div className={styles.imgButton}>
+                            <img
+                              src={item.img}
+                              alt={item.title}
+                              className={styles.songImg}
+                            />
+                            <div className={styles.songPlayButton}>
+                              <Link href={item.link}>
+                                <PlayCircleFilled />
+                              </Link>
+                            </div>
+                          </div>
+                          <div className={styles.songContent}>
+                            <div className={styles.songTitleSinger}>
+                              <div className={styles.songTitle}>
+                                {item.title}
+                              </div>
+                              <div className={styles.songSingers}>
+                                {item.singers}
+                              </div>
+                            </div>
+                            <div></div>
+                          </div>
+                        </li>
+                      ))}
+                    </>
+                  )}
+
+                  {!checked && checkedEnglish && (
+                    <>
+                      {data.songList
+                        .filter((item) => item.category == "English")
+                        .map((item, i) => (
+                          <li key={i} className={styles.songItem}>
+                            <div className={styles.imgButton}>
+                              <img
+                                src={item.img}
+                                alt={item.title}
+                                className={styles.songImg}
+                              />
+                              <div className={styles.songPlayButton}>
+                                <Link href={item.link}>
+                                  <PlayCircleFilled />
+                                </Link>
+                              </div>
+                            </div>
+                            <div className={styles.songContent}>
+                              <div className={styles.songTitleSinger}>
+                                <div className={styles.songTitle}>
+                                  {item.title}
+                                </div>
+                                <div className={styles.songSingers}>
+                                  {item.singers}
+                                </div>
+                              </div>
+                              <div></div>
+                            </div>
+                          </li>
+                        ))}
+                    </>
+                  )}
+
+                  {checked && !checkedEnglish && (
+                    <>
+                      {data.songList
+                        .filter((item) => item.category == "Korean")
+                        .map((item, i) => (
+                          <li key={i} className={styles.songItem}>
+                            <div className={styles.imgButton}>
+                              <img
+                                src={item.img}
+                                alt={item.title}
+                                className={styles.songImg}
+                              />
+                              <div className={styles.songPlayButton}>
+                                <Link href={item.link}>
+                                  <PlayCircleFilled />
+                                </Link>
+                              </div>
+                            </div>
+                            <div className={styles.songContent}>
+                              <div className={styles.songTitleSinger}>
+                                <div className={styles.songTitle}>
+                                  {item.title}
+                                </div>
+                                <div className={styles.songSingers}>
+                                  {item.singers}
+                                </div>
+                              </div>
+                              <div></div>
+                            </div>
+                          </li>
+                        ))}
+                    </>
+                  )}
+                </ul>
+              </>
             ) : null}
 
             <p>
@@ -173,60 +317,70 @@ export async function getStaticProps({ params }) {
           singers: "Christopher, CHUNG HA",
           img: "/images/songs/whenigetold.png",
           link: "https://open.spotify.com/track/5f2CcxzZoW7hNs1O8NhG6y?si=476de78a032c48c1",
+          category: "KPOP",
         },
         {
           title: "Dear My Winter",
           singers: "george, Chuu",
           img: "/images/songs/dearmywinter.png",
           link: "https://open.spotify.com/track/7sqtyHfTHtkLtnxuQ3ttvo?si=db6a92f97dd644d1",
+          category: "Korean",
         },
         {
           title: "Lovey Dovey",
           singers: "BIG Naughty, meenoi",
           img: "/images/songs/loveydovey.png",
           link: "https://open.spotify.com/track/1s3AJx7XASsPSA2cKJdXG6?si=2c1a58e5903043df",
+          category: "Korean",
         },
         {
           title: "Love Song",
           singers: "Wonstein, JOY",
           img: "/images/songs/lovesong.png",
           link: "https://open.spotify.com/track/7JF6USn1d7oBnjITCKtSHp?si=0555b38487324495",
+          category: "Korean",
         },
         {
           title: "I Wanna Be - The 1st Album Repackage",
           singers: "KEY, JEON SOYEON",
           img: "/images/songs/iwannabe.png",
           link: "https://open.spotify.com/track/7Bd6h5KwA4ASCXCSoWIS3i?si=9bcf0224f87a4e70",
+          category: "Korean",
         },
         {
           title: "I Like Me Better",
           singers: "Lauv",
           img: "/images/songs/ilikemebetter.png",
           link: "https://open.spotify.com/track/0EcQcdcbQeVJn9fknj44Be",
+          category: "English",
         },
         {
           title: "Stay",
           singers: "The Kid LAROI, Justin Bieber",
           img: "/images/songs/stay.png",
           link: "https://open.spotify.com/track/5HCyWlXZPP0y6Gqq8TgA20?si=ddde4a85cf3045be",
+          category: "English",
         },
         {
           title: "In the Name of Love",
           singers: "Martin Garrix",
           img: "/images/songs/inthenameoflove.png",
           link: "https://open.spotify.com/track/23L5CiUhw2jV1OIMwthR3S?si=16e454ecf7fe4357",
+          category: "English",
         },
         {
           title: "Let Me Love You",
           singers: "DJ Snake, Justin Bieber",
           img: "/images/songs/letmeloveyou.png",
           link: "https://open.spotify.com/track/0lYBSQXN6rCTvUZvg9S0lU?si=b7479e32c785404f",
+          category: "English",
         },
         {
           title: "Way U Are",
           singers: "Jiselle, BIG Naughty",
           img: "/images/songs/wayuare.png",
           link: "https://open.spotify.com/track/1cHwmkEYWHstlT9hRF0EeP?si=212ea56e2b644d52",
+          category: "Korean",
         },
       ],
     },
