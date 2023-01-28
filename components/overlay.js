700d7f229953df6ec24d5c3dc8c878dae56d3410
phuclinh9802@gmail.com <anicedream2002@gmail.com>
2023-01-28 13:24:50 -0600

    fix ui bug - ThreeJS

--- components/overlay.js
+++ components/overlay.js
@@ -4,8 +4,8 @@ import styles from "../styles/overlay.module.css";
 export default function Overlay() {
   return (
     <div className={styles.container}>
-      <h1>Spin Me!</h1>
-      <p>Try it, it is really fun!!!</p>
+      <h1 className={styles.h1}>Spin Me!</h1>
+      <p className={styles.p}>Try it, it is really fun!!!</p>
     </div>
   );
 }
